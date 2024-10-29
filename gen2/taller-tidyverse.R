773c88bc072bb24fd1fa6fc534592f87cd4f82b3
paulovillarroel <pvillarroeltapia@gmail.com>
2024-10-29 19:33:55 -0300

    Upload workshop

---
+++ gen2/taller-tidyverse.R
@@ -0,0 +1,33 @@
+## Taller librería Tidyverse Hazla con Datos - Curso Ciencia de Datos en Salud
+
+# Cargue la librería de Tidyverse
+
+# Cargue el dataset ENO (Enfermedades Notificables Obligatorias) disponible en el siguiente link: https://datos.gob.cl/dataset/76aa7b9b-8a3b-404a-853d-0ecff159876d/resource/b6081caf-39af-4f3f-86de-d6e00739bdc4/download/20240304_base_eno_final.csv
+
+# Guárdelo en un objeto llamado 'eno' 
+
+
+## Desarrolle las siguiente preguntas:
+
+# ¿Cuántas columnas y filas tiene el dataset?
+
+# ¿Cuáles son los nombres de las columnas?
+
+# ¿Cuáles son los tipos de datos de cada columna?
+
+# Elimina todas las columnas con valores vacios (NA´s) y sobreescribe el dataset original
+
+# ¿Cuál es el rango de años cubierto por el dataset (de las notificaciones)?
+
+# ¿Qué enfermedades están incluidas en el dataset?
+
+# ¿Cuántos casos de cada enfermedad se registran en el dataset?
+
+# Para el año 2022, ¿Cuál es la región con más casos registrados de Coqueluche y cuántos casos son?
+
+# Calcula el promedio de notificaciones por año para cada región
+
+# ¿Cuál es la región con más casos registrados en el dataset (de cualquier enfermedad) y cuántos casos son?
+
+# ¿Cuáles son las 3 enfermedades con más casos registrados por cada año?
+
